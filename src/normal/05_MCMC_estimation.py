c0ce81e6ce885eaf16fee4319abc9f8b6d360e38
Joshua Görner <joshuagorner@Le-Big-Mac.local>
2018-01-02 14:08:33 +0100

    implemented MCMC for normal distribution

--- src/normal/05_MCMC_estimation.py
+++ src/normal/05_MCMC_estimation.py
@@ -1,6 +1,7 @@
 # IMPORTS
+import pymc3 as pm
 import numpy as np
-import scipy.stats as stats
+from scipy.stats import norm
 import matplotlib.pyplot as plt
 import matplotlib.style as style
 from IPython.core.display import HTML
@@ -20,29 +21,38 @@ HTML("""
 """)
 plt.figure(dpi=100)
 
-##### COMPUTATION #####
-# DECLARING THE "TRUE" PARAMETERS UNDERLYING THE SAMPLE
-mu_real = 10
-sigma_real = 2
+##### SIMULATION #####
+# MODEL BUILDING
+with pm.Model() as model:
+    mu = pm.Uniform("mu", upper=20)
+    std = pm.Uniform("std", upper=5)
+    normal = pm.Normal("normal", mu=mu, sd=std, observed=sample)
+    
+# MODEL RUN
+with model:
+    step = pm.Metropolis()
+    trace = pm.sample(50000, step=step)
+    burned_trace = trace[45000:]
+    
+# MU - 95% CONF INTERVAL
+mus = burned_trace["mu"]
+mu_est_95 = np.mean(mus) - 2*np.std(mus), np.mean(mus) + 2*np.std(mus)
+print("95% of sampled mus are between {:0.3f} and {:0.3f}".format(*mu_est_95))
 
-# DRAW A SAMPLE OF N=1000
-np.random.seed(42)
-sample = stats.norm.rvs(loc=mu_real, scale=sigma_real, size=1000)
+# STD - 95% CONF INTERVAL
+stds = burned_trace["std"]
+std_est_95 = np.mean(stds) - 2*np.std(stds), np.mean(stds) + 2*np.std(stds)
+print("95% of sampled sigmas are between {:0.3f} and {:0.3f}".format(*std_est_95))
 
-# ESTIMATE MU AND SIGMA
-mu_est = np.mean(sample)
-sigma_est = np.std(sample)
-print("Estimated MU: {}\nEstimated SIGMA: {}".format(mu_est, sigma_est))
-
-##### PLOTTING #####
+#### PLOTTING #####
 # SAMPLE DISTRIBUTION
 plt.hist(sample, bins=50,normed=True, alpha=.25)
 
 # TRUE CURVE
 plt.plot(np.linspace(2, 18, 1000), norm.pdf(np.linspace(2, 18, 1000),loc=mu_real, scale=sigma_real))
 
-# ESTIMATED CURVE
-plt.plot(np.linspace(2, 18, 1000), norm.pdf(np.linspace(2, 18, 1000),loc=np.mean(sample), scale=np.std(sample)))
+# ESTIMATED CURVE MCMC
+plt.plot(np.linspace(2, 18, 1000), norm.pdf(np.linspace(2, 18, 1000),loc=np.mean(mus), scale=np.mean(stds)))
 
 # LEGEND
 plt.text(x=9.5, y=.1, s="sample", alpha=.75, weight="bold", color="#008fd5")
@@ -54,10 +64,10 @@ plt.tick_params(axis = 'both', which = 'major', labelsize = 18)
 plt.axhline(y = 0, color = 'black', linewidth = 1.3, alpha = .7)
 
 # TITLE, SUBTITLE & FOOTER
-plt.text(x = 0, y = 0.3, s = "Normal Distribution",
+plt.text(x = 0, y = 0.3, s = "Normal Distribution - Parameter Estimation (MCMC)",
                fontsize = 26, weight = 'bold', alpha = .75)
 plt.text(x = 0, y = 0.265, 
-         s = 'Depicted below is the distribution of a sample (blue) drawn from a normal distribution with $\mu = 10$\nand $\sigma = 2$ (red). Also the estimated distrubution with $\mu \sim {:.3f} $ and $\sigma \sim {:.3f} $ is shown (yellow).'.format(np.mean(sample), np.std(sample)),
+         s = 'Depicted below is the distribution of a sample (blue) drawn from a normal distribution with $\mu = 10$\nand $\sigma = 2$ (red). Also the estimated distrubution with $\mu \sim {:.3f} $ and $\sigma \sim {:.3f} $ is shown (yellow).'.format(np.mean(mus), np.mean(stds)),
          fontsize = 19, alpha = .85)
 plt.text(x = 0,y = -0.025,
          s = '   ©Joshua Görner                                                                                                                                                   github.com/jgoerner   ',
